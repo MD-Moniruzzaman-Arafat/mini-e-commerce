6b49e2509708e0f90c7ef6cc9ba61fc126973eba
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-07-07 21:37:52 +0600

    added navbar component

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,14 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../pages/HomePage/HomePage";
 import ProductDetailPage from "../pages/ProductDetailPage/ProductDetailPage";
+import MainLayout from "../layouts/MainLayout/MainLayout";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <HomePage />,
+        element: <MainLayout />,
+        children: [
+            {
+                path: "/",
+                element: <HomePage />,
+            },
+            {
+                path: "/product-detail",
+                element: <ProductDetailPage />
+            }
+        ]
     },
-    {
-        path: '/product-detail',
-        element: <ProductDetailPage />
-    }
 ]);
