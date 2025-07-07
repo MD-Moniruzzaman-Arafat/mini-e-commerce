b65e635bbbff5c41ecaa2342c6b815dc8491573b
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-07-07 23:28:46 +0600

    sidebar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,8 @@ export default function Navbar() {
     const menu = (
         <>
             <li><Link to={'/'}>Home</Link></li>
-            <li><a>Cart</a></li>
+            <li><label htmlFor="my-drawer-4"><a>Cart</a></label></li>
+
         </>
     )
     return (
@@ -23,13 +24,37 @@ export default function Navbar() {
                                 {menu}
                             </ul>
                         </div>
-                        <a className="btn btn-ghost text-xl">Mini-E-Commerce</a>
+                        <Link to={'/'} className="btn btn-ghost text-xl">Mini-E-Commerce</Link>
                     </div>
                     <div className="navbar-end hidden lg:flex">
                         <ul className="menu menu-horizontal px-1 font-medium">
                             {menu}
                         </ul>
                     </div>
+                </div>
+            </div>
+            <div className="drawer drawer-end">
+                <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
+                    <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+                        {/* Sidebar content here */}
+                        <div className='flex justify-end'>
+                            <li className=''><label htmlFor="my-drawer-4"><a>
+                                <svg
+                                    className="swap-on fill-current"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="32"
+                                    height="32"
+                                    viewBox="0 0 512 512">
+                                    <polygon
+                                        points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+                                </svg>
+                            </a></label></li>
+                        </div>
+                        <li><a>Sidebar Item 1</a></li>
+                        <li><a>Sidebar Item 2</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
