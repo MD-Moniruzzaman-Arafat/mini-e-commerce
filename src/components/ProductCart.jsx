a337021e1fdeb21ba63aed52e87a2b596815ef0c
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-07-09 01:02:33 +0600

    checkout form design

--- src/components/ProductCart.jsx
+++ src/components/ProductCart.jsx
@@ -38,12 +38,38 @@ export default function ProductCart({ cartProduct }) {
                             <button className="join-item btn btn-xs">{productQuantity}</button>
                             <button onClick={handleIncrease} className="join-item btn btn-xs">+</button>
                         </div>
-                        <div className="badge badge-outline text-xs cursor-pointer hover:bg-green-500 hover:text-white transition-colors duration-200">
+                        <div onClick={() => document.getElementById('my_modal_3').showModal()} className="badge badge-outline text-xs cursor-pointer hover:bg-green-500 hover:text-white transition-colors duration-200">
                             Checkout
                         </div>
                     </div>
                 </div>
             </div>
+
+
+            {/* You can open the modal using document.getElementById('ID').showModal() method */}
+            {/* <button className="btn" onClick={() => document.getElementById('my_modal_3').showModal()}>open modal</button> */}
+            <dialog id="my_modal_3" className="modal">
+                <div className="modal-box">
+                    <form method="dialog">
+                        {/* if there is a button in form, it will close the modal */}
+                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend">Name</legend>
+                            <input type="text" className="input w-full" placeholder="Name...." />
+                        </fieldset>
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend">Email</legend>
+                            <input type="email" className="input w-full" placeholder="Email...." />
+                        </fieldset>
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend">Address</legend>
+                            <textarea className="textarea h-24 w-full" placeholder="Address...."></textarea>
+                        </fieldset>
+                        <button className="badge badge-outline text-xs cursor-pointer hover:bg-blue-500 hover:text-white transition-colors duration-200">Submit</button>
+                    </form>
+                </div>
+            </dialog>
         </div>
     )
 }
